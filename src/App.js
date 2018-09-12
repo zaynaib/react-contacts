005514ed5c7cfb43d1a80e8aef6a5be38e481bb7
Zaynaib Giwa <zaynaibg@gmail.com>
2018-09-12 17:38:35 -0500

    created ListContacts componenet to show contacts in the dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+//import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
-import CreateContact from './CreateContact'
+//import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
+const contacts = [
+    {
+      "id": "ryan",
+      "name": "Ryan Florence",
+      "email": "ryan@reacttraining.com",
+      "avatarURL": "http://localhost:5001/ryan.jpg"
+    },
+    {
+      "id": "michael",
+      "name": "Michael Jackson",
+      "email": "michael@reacttraining.com",
+      "avatarURL": "http://localhost:5001/michael.jpg"
+    },
+    {
+      "id": "tyler",
+      "name": "Tyler McGinnis",
+      "email": "tyler@reacttraining.com",
+      "avatarURL": "http://localhost:5001/tyler.jpg"
+    }
+  ]
 
 class App extends Component {
 
 
   render() {
     return (
-      <div>
-        
-        Hello World!
-      </div>
+      <ListContacts contacts ={contacts}/>
     )
   }
 }
