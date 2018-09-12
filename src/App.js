f105906a08126318d4dfab75640fd7312fe361aa
Zaynaib Giwa <zaynaibg@gmail.com>
2018-09-12 18:06:43 -0500

    added a state to the contacts app componenet

--- src/App.js
+++ src/App.js
@@ -3,33 +3,36 @@ import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 //import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
-const contacts = [
-    {
-      "id": "ryan",
-      "name": "Ryan Florence",
-      "email": "ryan@reacttraining.com",
-      "avatarURL": "http://localhost:5001/ryan.jpg"
-    },
-    {
-      "id": "michael",
-      "name": "Michael Jackson",
-      "email": "michael@reacttraining.com",
-      "avatarURL": "http://localhost:5001/michael.jpg"
-    },
-    {
-      "id": "tyler",
-      "name": "Tyler McGinnis",
-      "email": "tyler@reacttraining.com",
-      "avatarURL": "http://localhost:5001/tyler.jpg"
-    }
-  ]
+
 
 class App extends Component {
-
-
+    state ={
+        //piece of state to render UI it should live inside of a component
+        contacts:[
+            {
+              "id": "ryan",
+              "name": "Ryan Florence",
+              "email": "ryan@reacttraining.com",
+              "avatarURL": "http://localhost:5001/ryan.jpg"
+            },
+            {
+              "id": "michael",
+              "name": "Michael Jackson",
+              "email": "michael@reacttraining.com",
+              "avatarURL": "http://localhost:5001/michael.jpg"
+            },
+            {
+              "id": "tyler",
+              "name": "Tyler McGinnis",
+              "email": "tyler@reacttraining.com",
+              "avatarURL": "http://localhost:5001/tyler.jpg"
+            }
+        ]
+    }
   render() {
     return (
-      <ListContacts contacts ={contacts}/>
+        //instead of props it will be a state
+      <ListContacts contacts ={this.state.contacts}/>
     )
   }
 }
