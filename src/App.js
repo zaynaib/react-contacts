e83a8d23b2af81292d9a8c1a087291ec8f880480
Zaynaib Giwa <zaynaibg@gmail.com>
2018-09-12 13:40:58 -0500

    added starter files for contacts tutorial on react state;

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
+import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
+import * as ContactsAPI from './utils/ContactsAPI'
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+        
+        Hello World!
+      </div>
+    )
+  }
+}
+
+export default App;
